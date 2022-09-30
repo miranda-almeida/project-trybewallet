021aa6084ea24c7ec4322f796b47adfe2246efe9
miranda-almeida <magui@duck.com>
2022-09-30 18:30:56 -0300

    requisito 7

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,7 +1,14 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 class Table extends Component {
+  askToFixed = (ask) => Number(ask).toFixed(2);
+
+  compile = (ask, value) => (Number(ask) * Number(value)).toFixed(2);
+
   render() {
+    const { expenses } = this.props;
     return (
       <table>
         {/* ajustando nomenclatura de tags a partir do erro indicado no console. ref: https://stackoverflow.com/questions/43958700/react-error-th-cannot-appear-as-a-child-of-thead-see-unknown-thead-th */}
@@ -18,9 +25,35 @@ class Table extends Component {
             <th>Editar/Excluir</th>
           </tr>
         </thead>
+
+        <tbody>
+          { expenses.map((element) => (
+            <tr key={ element.id }>
+              <td>{ element.description }</td>
+              <td>{ element.tag }</td>
+              <td>{ element.method }</td>
+              <td>{ Number(element.value).toFixed(2) }</td>
+              <td>{ element.exchangeRates[element.currency].name }</td>
+              <td>{ this.askToFixed(element.exchangeRates[element.currency].ask) }</td>
+              <td>
+                { this.compile(element.exchangeRates[element
+                  .currency].ask, element.value) }
+              </td>
+              <td>Real</td>
+            </tr>
+          ))}
+        </tbody>
       </table>
     );
   }
 }
 
-export default Table;
+const mapStateToProps = ({ wallet }) => ({
+  expenses: wallet.expenses,
+});
+
+Table.propTypes = {
+  expenses: PropTypes.string,
+}.isRequired;
+
+export default connect(mapStateToProps)(Table);
